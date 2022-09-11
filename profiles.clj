9c231d98a952ba329e608d7d5a35fc69438454f1
MelKori <mieldo@gmail.com>
2022-09-11 22:40:14 +0300

    update project structure, update dependencies, bumped version

--- profiles.clj
+++ profiles.clj
@@ -5,20 +5,17 @@
             :javac-options     ["-source" "9" "-target" "9" "-g:none"]
             :jar-exclusions    [#"\.java"]}
  :dev      {:dependencies        [[org.clojure/clojure "1.11.1"]
-                                  [org.slf4j/slf4j-api "1.7.36"]
-                                  [ch.qos.logback/logback-classic "1.2.11"]
-                                  [org.postgresql/postgresql "42.4.1"]
+                                  [org.slf4j/slf4j-api "2.0.0"]
+                                  [ch.qos.logback/logback-classic "1.4.0"]
+                                  [org.postgresql/postgresql "42.5.0"]
                                   ;; Loggers
                                   ;; -------
                                   [com.taoensso/timbre "5.2.1"]
                                   [com.fzakaria/slf4j-timbre "0.3.21"]
-                                  [org.slf4j/slf4j-api "1.7.36"]
-                                  [org.slf4j/log4j-over-slf4j "1.7.36"]
-                                  [org.slf4j/jul-to-slf4j "1.7.36"]
-                                  [org.slf4j/jcl-over-slf4j "1.7.36"]]
+                                  [org.slf4j/slf4j-api "2.0.0"]
+                                  [org.slf4j/log4j-over-slf4j "2.0.0"]
+                                  [org.slf4j/jul-to-slf4j "2.0.0"]
+                                  [org.slf4j/jcl-over-slf4j "2.0.0"]]
             :java-source-paths   ["java"]
-            :plugins             [[org.apache.maven.wagon/wagon-ssh-external "3.5.2"]
-                                  [org.apache.maven.wagon/wagon-http-lightweight "3.5.2"]]
-            :deploy-repositories [["private-jars-scp" {:url              "scp://local.repo/home/clojar/data/dev_repo/"
-                                                       :username         "clojar"
-                                                       :private-key-file :env/clojure_ssh_key}]]}}
+            :plugins             []
+            :deploy-repositories []}}
--- project.clj
+++ project.clj
@@ -1,14 +1,9 @@
-(defproject com.dkdhub/qtasks "0.5.1"
+(defproject com.dkdhub/qtasks "1.0.0"
   :description "DKD Quartz Library for Clojure"
   :url "http://dkdhub.com"
-  :license {:name "Proprietary"
-            :url  "http://dkdhub.com/licenses/base.html"}
+  :license {:name "Dual MIT & Proprietary"}
   :dependencies [[org.quartz-scheduler/quartz "2.3.2"]
                  [org.quartz-scheduler/quartz-jobs "2.3.2"]
                  [com.stuartsierra/component "1.1.0"]
                  [prismatic/plumbing "0.6.0"]
                  [org.clojure/core.async "1.5.648"]])
-
-(cemerick.pomegranate.aether/register-wagon-factory!
-  "scp" #(let [c (resolve 'org.apache.maven.wagon.providers.ssh.external.ScpExternalWagon)]
-           (clojure.lang.Reflector/invokeConstructor c (into-array []))))
